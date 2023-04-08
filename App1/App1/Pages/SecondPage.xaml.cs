3aea03900995c37146fa8aeef949405a117f46dc
Théo <theo.lavauzelle@etu.unilim.fr>
2023-04-08 20:21:30 +0200

    Implement second page

--- App1/App1/Pages/SecondPage.xaml.cs
+++ App1/App1/Pages/SecondPage.xaml.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using App1.Spotify_Service;
+using SpotifyAPI.Web;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -15,6 +16,16 @@ namespace App1.Pages
         public SecondPage()
         {
             InitializeComponent();
+            var artist = SpotifyService.Spotify.Artists.Get("1WgXqy2Dd70QQOU7Ay074N").Result;
+            Name.Text = artist.Name;
+            Followers.Text = artist.Followers.Total.ToString() + " followers";
+            Image.Source = artist.Images[0].Url;
+            Genres.Text = "Genres: ";
+            foreach (var genre in artist.Genres)
+            {
+                Genres.Text += genre + ", ";
+            }
+            Popularity.Text = "Rang: " + artist.Popularity;
         }
     }
 }
---
+++ App1/App1/Spotify Service/SpotifyService.cs
@@ -0,0 +1,15 @@
+﻿using SpotifyAPI.Web;
+
+namespace App1.Spotify_Service
+{
+    public static class SpotifyService
+    {
+        public static SpotifyClient Spotify { get; }
+
+        static SpotifyService()
+        {
+            var spotify = new SpotifyClient("METTRE SON TOKEN ICI");
+            Spotify = spotify;
+        }
+    }
+}
