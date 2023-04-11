7d9caecaa9671122058bdea2edb1f6aa72d4c568
Théo <theo.lavauzelle@etu.unilim.fr>
2023-04-12 00:28:15 +0200

    Replace code behind of first page by a ViewModel

--- App1/App1/Pages/FirstPage.xaml.cs
+++ App1/App1/Pages/FirstPage.xaml.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using App1.Spotify_Service;
+using App1.ViewModels;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -12,25 +13,30 @@ namespace App1.Pages
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class FirstPage : ContentPage
     {
+        private HomeViewModel _viewModel;
         public FirstPage()
         {
             InitializeComponent();
+            _viewModel = new HomeViewModel();
+            BindingContext = _viewModel;
+            //SpotifyService.Instance.ConnectSpotify();
         }
 
         async void click(object sender, EventArgs args)
         {
-            var ListeCases = new List<BoxView> { FirstCase, SecondCase, ThirdCase, FourthCase, FifthCase, SixthCase, SeventhCase, EighthCase, NinthCase, TenthCase, EleventhCase, TwelfthCase, ThirteenthCase, FourteenthCase, FifteenthCase, SixteenthCase, SeventeenthCase, EightteenthCase, NineteenthCase, TwentythCase };
-            for (int i = 0; i < ListeCases.Count; i++)
-            {
-                if (ListeCases[i].Color == Color.Blue)
-                {
-                    ListeCases[i].Color = Color.Black;
-                }
-                else
-                {
-                    ListeCases[i].Color = Color.Blue;
-                }
-            }
+            // var ListeCases = new List<BoxView> { FirstCase, SecondCase, ThirdCase, FourthCase, FifthCase, SixthCase, SeventhCase, EighthCase, NinthCase, TenthCase, EleventhCase, TwelfthCase, ThirteenthCase, FourteenthCase, FifteenthCase, SixteenthCase, SeventeenthCase, EightteenthCase, NineteenthCase, TwentythCase };
+            // for (int i = 0; i < ListeCases.Count; i++)
+            // {
+            //     if (ListeCases[i].Color == Color.Blue)
+            //     {
+            //         ListeCases[i].Color = Color.Black;
+            //     }
+            //     else
+            //     {
+            //         ListeCases[i].Color = Color.Blue;
+            //     }
+            // }
+            _viewModel.InvertColor();
         }
     }
 }
---
+++ App1/App1/ViewModels/HomeViewModel.cs
@@ -0,0 +1,36 @@
+﻿using Xamarin.Forms;
+
+namespace App1.ViewModels
+{
+    public class HomeViewModel: BaseViewModel
+    {
+        Color _oddCaseColor;
+        Color _evenCaseColor;
+        
+        public HomeViewModel()
+        {
+            OddCaseColor = Color.FromHex("#000000");
+            EvenCaseColor = Color.FromHex("#0000FF");
+        }
+        
+        public Color OddCaseColor
+        {
+            get { return GetValue<Color>(); }
+            set { SetValue(value); }
+        }
+        
+        public Color EvenCaseColor
+        {
+            get { return GetValue<Color>(); }
+            set { SetValue(value); }
+        }
+        
+        
+        public void InvertColor()
+        {
+            Color temp = OddCaseColor;
+            OddCaseColor = EvenCaseColor;
+            EvenCaseColor = temp;
+        }
+    }
+}
