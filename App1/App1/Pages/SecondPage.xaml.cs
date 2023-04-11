0ba951825489f879030ed0494eebd7bb6f86d45f
Théo <theo.lavauzelle@etu.unilim.fr>
2023-04-11 19:52:25 +0200

    Replace code behin with a ViewModel for second page

--- App1/App1/Pages/SecondPage.xaml.cs
+++ App1/App1/Pages/SecondPage.xaml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using App1.Spotify_Service;
+using App1.ViewModels;
 using SpotifyAPI.Web;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
@@ -13,19 +14,28 @@ namespace App1.Pages
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class SecondPage : ContentPage
     {
+        private ArtistViewModel _viewModel;
         public SecondPage()
         {
             InitializeComponent();
-            var artist = SpotifyService.Spotify.Artists.Get("1WgXqy2Dd70QQOU7Ay074N").Result;
-            Name.Text = artist.Name;
-            Followers.Text = artist.Followers.Total.ToString() + " followers";
-            Image.Source = artist.Images[0].Url;
-            Genres.Text = "Genres: ";
-            foreach (var genre in artist.Genres)
-            {
-                Genres.Text += genre + ", ";
-            }
-            Popularity.Text = "Rang de popularité: " + artist.Popularity;
+            // var artist = SpotifyService.Spotify.Artists.Get("1WgXqy2Dd70QQOU7Ay074N").Result;
+            // Name.Text = artist.Name;
+            // Followers.Text = artist.Followers.Total.ToString() + " followers";
+            // Image.Source = artist.Images[0].Url;
+            // Genres.Text = "Genres: ";
+            // foreach (var genre in artist.Genres)
+            // {
+            //     Genres.Text += genre + ", ";
+            // }
+            // Popularity.Text = "Rang: " + artist.Popularity;
+            _viewModel = new ArtistViewModel();
+            BindingContext = _viewModel;
+        }
+        
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+            await _viewModel.LoadData();
         }
     }
 }
---
+++ App1/App1/ViewModels/ArtistViewModel.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using App1.Spotify_Service;
+
+namespace App1.ViewModels
+{
+    public class ArtistViewModel: INotifyPropertyChanged
+    {
+        private string _followers;
+        public string Followers
+        {
+            get => _followers;
+            set => SetProperty(ref _followers, value);
+        }
+
+        private string _imageSource;
+        public string ImageSource
+        {
+            get => _imageSource;
+            set => SetProperty(ref _imageSource, value);
+        }
+
+        private string _genres;
+        public string Genres
+        {
+            get => _genres;
+            set => SetProperty(ref _genres, value);
+        }
+
+        private string _popularity;
+        public string Popularity
+        {
+            get => _popularity;
+            set => SetProperty(ref _popularity, value);
+        }
+        
+        private string _name;
+        public string Name
+        {
+            get => _name;
+            set => SetProperty(ref _name, value);
+        }
+
+        public async Task LoadData()
+        {
+            var artist = await SpotifyService.Spotify.Artists.Get("1WgXqy2Dd70QQOU7Ay074N");
+            Name = artist.Name;
+            Followers = artist.Followers.Total.ToString() + " followers";
+            ImageSource = artist.Images[0].Url;
+            Genres = "Genres: " + string.Join(", ", artist.Genres);
+            Popularity = "Rang: " + artist.Popularity;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void SetProperty<T>(ref T backingStore, T value, [CallerMemberName] string propertyName = "", Action onChanged = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(backingStore, value))
+                return;
+
+            backingStore = value;
+
+            onChanged?.Invoke();
+
+            OnPropertyChanged(propertyName);
+        }
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
