aa32a043ce145f8e5bd61542da8f96aa50f19ccb
Théo <theo.lavauzelle@etu.unilim.fr>
2023-04-12 00:29:23 +0200

    Add a playslist on fourth page which can update webview and can listen them

--- App1/App1/Pages/FourthPage.xaml.cs
+++ App1/App1/Pages/FourthPage.xaml.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using App1.Spotify_Service;
+using App1.ViewModels;
+using SpotifyAPI.Web;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -12,10 +14,38 @@ namespace App1.Pages
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class FourthPage : ContentPage
     {
+        private ExtraFeaturesViewModel _viewModel;
         public FourthPage()
         {
             InitializeComponent();
+            _viewModel = new ExtraFeaturesViewModel();
+            //BindingContext = _viewModel;
+            //TracksList.ItemsSource = _viewModel.Playlist.Tracks.Items;
             WebView.Source = SpotifyService.EmbeddedPreview("4xxLwHxyzJkg6Z002YRabT");
+            var playlist = SpotifyService.Spotify.Playlists.Get("37i9dQZF1DZ06evO15Ttp6").Result.Tracks.Items;
+            TracksList.ItemsSource = playlist.Select(t => t.Track);
+        }
+        
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+            await _viewModel.LoadData();
+        }
+        
+        private void OnTrackTapped(object sender, ItemTappedEventArgs e)
+        {
+            // Récupérer l'objet Track sélectionné
+            var selectedTrack = e.Item as FullTrack;
+
+            // Vérifier que l'objet n'est pas nul
+            if (selectedTrack != null)
+            {
+                // Appeler votre fonction avec l'ID de la Track en paramètre
+                WebView.Source = SpotifyService.EmbeddedPreview(selectedTrack.Id);
+            }
+
+            // Désélectionner l'élément sélectionné
+            ((ListView)sender).SelectedItem = null;
         }
     }
 }
---
+++ App1/App1/ViewModels/ExtraFeaturesViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System.Threading.Tasks;
+using App1.Spotify_Service;
+using SpotifyAPI.Web;
+using Xamarin.Forms;
+
+namespace App1.ViewModels
+{
+    public class ExtraFeaturesViewModel: BaseViewModel
+    {
+        private HtmlWebViewSource _htmlSource;
+        public HtmlWebViewSource HtmlSource
+        {
+            get => GetValue<HtmlWebViewSource>();
+            set => SetValue(value);
+        }
+        
+        private FullPlaylist _playlist;
+        public FullPlaylist Playlist
+        {
+            get => GetValue<FullPlaylist>();
+            set => SetValue(value);
+        }
+        
+        public ExtraFeaturesViewModel()
+        {
+            HtmlSource = SpotifyService.EmbeddedPreview("1WgXqy2Dd70QQOU7Ay074N");
+        }
+        
+        public async Task LoadData()
+        {
+            Playlist = await SpotifyService.Spotify.Playlists.Get("37i9dQZF1DZ06evO15Ttp6");
+            // var bip = Playlist.Tracks.Items[0].Track;
+            // Playlist.Tracks.Items[0]
+            
+        }
+    }
+}
